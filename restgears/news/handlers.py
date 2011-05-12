4db0eeeea9d250605ba0e4062c187e3e210454c0
abossard <abossard@gmail.com>
2011-05-12 16:42:46 +0200

    url, not image

--- restgears/news/handlers.py
+++ restgears/news/handlers.py
@@ -4,4 +4,4 @@ from news.models import Entry
 class EntryHandler(AnonymousBaseHandler):
    allowed_methods = ('GET',)
    model = Entry
-   fields = ('name', 'teaser', 'content', ('image_set',('image','description',),),'id','slug')
+   fields = ('name', 'teaser', 'content', ('image_set',('url','description',),),'id','slug')
