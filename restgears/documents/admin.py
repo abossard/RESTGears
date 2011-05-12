060043fc6c4797996136d419a3b0dce306223136
abossard <abossard@gmail.com>
2011-05-12 15:06:57 +0200

    fixed a nasty svn conflict....

--- restgears/documents/admin.py
+++ restgears/documents/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from restgears.documents.models import Document, Category, Tag
-=======
-from restgears.documents.models import Document, Category
->>>>>>> 3eba3c9fb2fb510c0733cf885cf9a66f99da3fb5
 
 class DocumentAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
@@ -14,11 +10,8 @@ class CategoryAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Category, CategoryAdmin)
-<<<<<<< HEAD
 
 class TagAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Tag, TagAdmin)
-=======
->>>>>>> 3eba3c9fb2fb510c0733cf885cf9a66f99da3fb5
