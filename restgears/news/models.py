68a4d19faf416f5643bc17d119891176c5e30e26
Andre Bossard <abossard@gmail.com>
2011-07-25 17:17:09 +0200

    added news scraper

--- restgears/news/models.py
+++ restgears/news/models.py
@@ -12,19 +12,20 @@ from google.appengine.api import images
 class Entry(BaseModel):
     teaser = models.TextField(max_length=500, help_text='This text is displayed in the news overview list a well as in top of every news entry (Text only)');
     content = models.TextField(max_length=4000, help_text='This text is the main content for the news entry (supports HTML).');
-    image = models.ImageField(upload_to='uploads/news', help_text='Select an image that is already resized for the iphone');
-    image_url = models.CharField(editable=False, max_length=255)
+    image = models.ImageField(upload_to='uploads/news', help_text='Select an image that is already resized for the iphone', null=True, blank=True);
+    image_url = models.CharField(editable=False, max_length=255, null=True, blank=True)
     thumb_image_url = models.CharField(editable=False, max_length=255)
-
+    external_key = models.CharField(editable=False, null=True, blank=True, max_length=255, db_index=True)
     def preview_image(self):
         return u'<img src="%s" alt="%s"/>'%(self.thumb_image_url, self.name)
     
     preview_image.allow_tags = True
 
     def save(self, force_insert=False, force_update=False, using=None):
-        blob_key = str(self.image.file.blobstore_info.key()) 
-        self.image_url = images.get_serving_url(blob_key, 960)
-        self.thumb_image_url = images.get_serving_url(blob_key,100)
+        if self.image:
+            blob_key = str(self.image.file.blobstore_info.key()) 
+            self.image_url = images.get_serving_url(blob_key, 960)
+            self.thumb_image_url = images.get_serving_url(blob_key,100)
         super(Entry, self).save(force_insert, force_update, using)
     
     class Meta:
